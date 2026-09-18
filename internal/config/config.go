3a0ab6b43728824dc149f339ffd9b42a57367ace
Usman Hassan <usmaanhassan@outlook.com>
2026-09-18 16:56:03 +0500

    feat(config): add public base URL setting

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,9 +3,10 @@ package config
 import "os"
 
 type Config struct {
-	Address    string
-	Database   string
-	GeoIPDB    string
+	Address string
+	Database string
+	GeoIPDB string
+	BaseURL string
 }
 
 func Load() Config {
@@ -18,8 +19,9 @@ func Load() Config {
 		database = "shorty.db"
 	}
 	return Config{
-		Address:  address,
+		Address: address,
 		Database: database,
-		GeoIPDB:  os.Getenv("SHORTY_GEOIP_DB"),
+		GeoIPDB: os.Getenv("SHORTY_GEOIP_DB"),
+		BaseURL: os.Getenv("SHORTY_BASE_URL"),
 	}
 }
