cce05e60dfffe30c22b473777f4005d390735aca
Usman Hassan <usmaanhassan@outlook.com>
2026-09-18 18:01:00 +0500

    fix(cli): clean command entrypoint

--- cmd/shorty-cli/main.go
+++ cmd/shorty-cli/main.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strconv"
 	"time"
 
 	"github.com/UsmanXTech/shorty/internal/cli"
@@ -16,15 +15,11 @@ func main() {
 	baseURL := os.Getenv("SHORTY_URL")
 	if baseURL == "" { baseURL = "http://localhost:8080" }
 	if len(os.Args) < 2 { usage(); os.Exit(2) }
-
 	client := cli.NewClient(baseURL)
 	switch os.Args[1] {
-	case "create":
-		create(client, os.Args[2:])
-	case "stats":
-		stats(client, os.Args[2:])
-	default:
-		usage(); os.Exit(2)
+	case "create": create(client, os.Args[2:])
+	case "stats": stats(client, os.Args[2:])
+	default: usage(); os.Exit(2)
 	}
 }
 
@@ -37,9 +32,14 @@ func create(client *cli.Client, args []string) {
 	fs.Parse(args)
 	if *rawURL == "" { fmt.Fprintln(os.Stderr, "create: --url is required"); os.Exit(2) }
 	link := links.Link{URL: *rawURL, Slug: *slug}
-	if *expires != "" { t, err := time.Parse(time.RFC3339, *expires); if err != nil { fmt.Fprintln(os.Stderr, "create: --expires-at must be RFC3339"); os.Exit(2) }; link.ExpiresAt = &t }
+	if *expires != "" {
+		t, err := time.Parse(time.RFC3339, *expires)
+		if err != nil { fmt.Fprintln(os.Stderr, "create: --expires-at must be RFC3339"); os.Exit(2) }
+		link.ExpiresAt = &t
+	}
 	if *maxClicks > 0 { link.MaxClicks = maxClicks } else if *maxClicks < 0 { fmt.Fprintln(os.Stderr, "create: --max-clicks must be positive"); os.Exit(2) }
-	out, err := client.CreateLink(link); if err != nil { fmt.Fprintln(os.Stderr, err); os.Exit(1) }
+	out, err := client.CreateLink(link)
+	if err != nil { fmt.Fprintln(os.Stderr, err); os.Exit(1) }
 	printJSON(out)
 }
 
@@ -51,8 +51,14 @@ func stats(client *cli.Client, args []string) {
 	interval := fs.String("interval", "hour", "aggregation interval: hour or day")
 	fs.Parse(args)
 	if *id <= 0 { fmt.Fprintln(os.Stderr, "stats: --id is required"); os.Exit(2) }
-	parse := func(raw string) time.Time { if raw == "" { return time.Time{} }; t, err := time.Parse(time.RFC3339, raw); if err != nil { fmt.Fprintln(os.Stderr, "stats: dates must be RFC3339"); os.Exit(2) }; return t }
-	out, err := client.Stats(*id, parse(*from), parse(*to), *interval); if err != nil { fmt.Fprintln(os.Stderr, err); os.Exit(1) }
+	parse := func(raw string) time.Time {
+		if raw == "" { return time.Time{} }
+		t, err := time.Parse(time.RFC3339, raw)
+		if err != nil { fmt.Fprintln(os.Stderr, "stats: dates must be RFC3339"); os.Exit(2) }
+		return t
+	}
+	out, err := client.Stats(*id, parse(*from), parse(*to), *interval)
+	if err != nil { fmt.Fprintln(os.Stderr, err); os.Exit(1) }
 	printJSON(out)
 }
 
@@ -64,5 +70,4 @@ func usage() {
 	fmt.Println("  shorty-cli create --url URL [--slug SLUG] [--expires-at RFC3339] [--max-clicks N]")
 	fmt.Println("  shorty-cli stats --id ID [--from RFC3339] [--to RFC3339] [--interval hour|day]")
 	fmt.Println("Environment: SHORTY_URL (default http://localhost:8080)")
-	_ = strconv.IntSize
 }
