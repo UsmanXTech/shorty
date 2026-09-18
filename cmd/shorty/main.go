7c3330c9c09e035a9ee6f69e40d08da6e3d17e20
Usman Hassan <usmaanhassan@outlook.com>
2026-09-18 09:38:28 +0500

    feat: connect server to SQLite

--- cmd/shorty/main.go
+++ cmd/shorty/main.go
@@ -6,15 +6,25 @@ import (
 	"os"
 
 	"github.com/UsmanXTech/shorty/internal/config"
+	"github.com/UsmanXTech/shorty/internal/database"
+	"github.com/UsmanXTech/shorty/internal/links"
 	"github.com/UsmanXTech/shorty/internal/server"
 )
 
 func main() {
 	cfg := config.Load()
 
-	srv := server.New(cfg)
-	log.Printf("shorty listening on %s", cfg.Address)
+	db, err := database.Open(cfg.Database)
+	if err != nil {
+		log.Printf("database startup failed: %v", err)
+		os.Exit(1)
+	}
+	defer db.Close()
 
+	repo := links.NewSQLiteRepository(db.DB)
+	srv := server.NewWithRepository(cfg, repo)
+
+	log.Printf("shorty listening on %s", cfg.Address)
 	if err := http.ListenAndServe(cfg.Address, srv.Handler()); err != nil {
 		log.Printf("server stopped: %v", err)
 		os.Exit(1)
